8fcb1e1963c9c012de76d8437ac93f0f6cfda7ae
Angela Loftus <angelaloftus@yahoo.com>
2021-11-12 13:53:21 -0600

    models

---
+++ models/Index.js
@@ -0,0 +1,8 @@
+//import models
+ const User = require('./User');
+ const Post = require('./Post');
+ const Comment = require('./Comment');
+
+ //create associations
+
+ module.exports = {User, Post, Comment}
